7eb04a12c6c02d50b5c6b304b3c8e022c4a03d34
Juls Lin <julianna.lin4@gmail.com>
2023-10-11 10:55:30 -0700

    fresh slate

--- main.js
+++ main.js
@@ -8,23 +8,3 @@ const { pipeline } = require("pipeline");
 
 
 
-
-ioHandlerHandler.unzip()
-    .then(() => ioHandler.readDir())
-    .then((arr) => ioHandler(arr))
-    .catch((err) => console.log(err));
-
-
-function readSomething() {
-    return New Promise ((resolve, reject) => {
-        pipeline(
-            fs.createReadStream("abc.txt"),
-            unzipper.Extract(),
-            (err) => console.log(err)
-        )
-    })
-}
-
-readSomething()
-.then()
-.catch()
